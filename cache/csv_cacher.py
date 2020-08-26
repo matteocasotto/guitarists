904d5403977de2a0796388f51c122790ea615143
Alberto Marchesini <alberto.marchesini@student.h-farm.com>
2020-08-26 11:38:13 +0200

    Added docstrings

--- cache/csv_cacher.py
+++ cache/csv_cacher.py
@@ -2,14 +2,29 @@ import csv
 
 
 class CsvCacher:
+    """Class that handles a cache and writes it to a file"""
+
     def __init__(self, filename='cache.csv'):
+        """Constructor
+
+        Parameters:
+            filename (str): The csv file where the cache will be stored
+        """
         self.cache = []
         self.filename = filename
 
     def add_to_cache(self, question, answer):
+        """
+        Add a question and its answer to the cache
+
+        Parameters:
+            question (str): The question
+            answer (str): The answer to the question
+        """
         self.cache.append((question, answer))
 
     def write_cache(self):
+        """Write cache to file"""
         with open(self.filename, 'w') as cache_file:
             writer = csv.writer(cache_file, delimiter=',')
             writer.writerow(['Question', 'Answer'])
